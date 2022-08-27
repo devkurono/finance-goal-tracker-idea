f0e0c2099d911b4314efee8e5d9d245002a96642
Justin Pascual <chrono.social@outlook.com>
2022-08-28 00:26:55 +0800

    :tada: Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const default_finance_list = {
+  title: "Work Salary",
+  salary: 2000,
+  salary_received: 0,
+  payment_status: [false, false],
+  date: new Date(),
+};
+
+const finance_list = [
+  {
+    title: "Work Salary",
+    salary: 2000,
+    salary_received: 0,
+    payment_status: [false, false],
+    date: new Date(),
+  },
+];
+
+console.log(finance_list);
