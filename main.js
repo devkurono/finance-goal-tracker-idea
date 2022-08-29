374ac91e4ea717be7277d9c6264a54364dcab29d
Kurono <chrono.social@outlook.com>
2022-08-29 22:03:38 +0800

    Update main.js

--- main.js
+++ main.js
@@ -8,25 +8,83 @@ function salaryStatus(salary, salary_received) {
   return (salary_received / salary_percentage) | 0;
 }
 
+function getTotalSalaryReceived() {
+  let total_salary_received = 0;
+  for (const financeListElement of finance_list) {
+    total_salary_received += financeListElement.salary_received;
+  }
+  return total_salary_received;
+}
+
+function getTotalSalary() {
+  let total_salary = 0;
+  for (const financeListElement of finance_list) {
+    total_salary += financeListElement.salary;
+  }
+  return total_salary;
+}
+
+function getSalaryReceivedPercentage() {
+  return (getTotalSalaryReceived() / (getTotalSalary() / 100)) | 0;
+}
+
+function getDate() {
+  return new Date().toLocaleDateString('en-us', {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+    weekday: 'short',
+  });
+}
+
+function getTime() {
+  return new Date().toLocaleTimeString();
+}
+
 const default_value = {
+  id: 1,
   title: '',
+  description: '',
   salary: 0,
   salary_received: 0,
-  salary_status: 0,
-  date: new Date(),
+  salary_progress: 0,
+  balance: 0,
+  received_logs: [
+    {
+      salary_received: 0,
+      date: getDate(),
+      time: getTime(),
+    },
+  ],
 };
 
 const finance_list = [];
 
-let received = 2_000;
-const add_finance_data = addData({
-  title: 'Work 2',
-  salary: 43_000,
-  salary_received: received,
-  salary_status: salaryStatus(43_000, received),
-});
+let received = 45_000;
 
-// Push data to finance list array
-finance_list.push(add_finance_data);
+finance_list.push(
+  addData({
+    id: 1,
+    title: 'Work 1',
+    salary: 43_000,
+    salary_received: received,
+    balance: 43_000 - received,
+    salary_progress: salaryStatus(43_000, received),
+  })
+);
+
+finance_list.push(
+  addData({
+    id: 2,
+    title: 'Work 2',
+    salary: 80_000,
+    salary_received: received,
+    balance: 80_000 - received,
+    salary_progress: salaryStatus(80_000, received),
+  })
+);
 
 console.log(finance_list);
+console.log('Total Salary: ', getTotalSalary());
+console.log('Total Salary Received: ', getTotalSalaryReceived());
+console.log('Total Salary Received Percentage:', getSalaryReceivedPercentage());
